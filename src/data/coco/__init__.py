da1dd9d95373faad3684d4d5e8a1c66a1ab86b0e
Ziheng Jin <starhtime0715@gmail.com>
2025-03-21 11:08:37 +0800

    support aircraft detection

--- src/data/coco/__init__.py
+++ src/data/coco/__init__.py
@@ -10,6 +10,13 @@ from .hrsid_dataset import (
     hrsid_category2name,
 )
 
+from .aircraft_dataset import (
+    AircraftDetection,
+    aircraft_category2name,
+    aircraft_category2label,
+    aircraft_label2category,
+)
+
 from .coco_eval import *
 
 from .coco_utils import get_coco_api_from_dataset
